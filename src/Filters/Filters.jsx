729a71f5455d2608820c232879ee9f0fa030a12d
serj <sergejbaturin1985@gmail.com>
2020-06-23 03:21:14 +0500

    Add price sorting handler to Filters component

--- src/Filters/Filters.jsx
+++ src/Filters/Filters.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
-const data = [
+const flowers = [
   { name: "Астра", product: "astor", stocklevel: "10", price: 2.99 },
   { name: "Нарцисс", product: "daffodil", stocklevel: "12", price: 1.99 },
   { name: "Роза", product: "rose", stocklevel: "2", price: 4.99 },
@@ -9,8 +9,18 @@ const data = [
   { name: "Примула", product: "primula", stocklevel: "1", price: 3.12 },
   { name: "Подснежник", product: "snowdrop", stocklevel: "15", price: 0.99 },
 ];
+const some = [
+  { name: "asdasd", product: "65543524213", stocklevel: "113", price: 10 },
+  { name: "zxczxc", product: "34534534", stocklevel: "234", price: 150 },
+  { name: "cacasc", product: "234234234", stocklevel: "34234", price: 30 },
+  { name: "ascasc", product: "123123123", stocklevel: "1", price: 20 },
+];
 
 const Filters = () => {
+  const [data, setData] = useState(flowers);
+  const [sortedData, setSortedData] = useState(null);
+
+
   return (
     <div className="filters">
       <h1>Filters</h1>
@@ -20,15 +30,17 @@ const Filters = () => {
             <td>Name</td>
             <td>Latin</td>
             <td>Level</td>
-            <td>Price</td>
+            <td onClick={() => setSortedData(data.sort((a, b) => b.price - a.price))}>
+              Price
+            </td>
           </tr>
         </thead>
         {data.map((item, i) => (
-          <tr>
-            <td key={i}>{item.name}</td>
-            <td key={i}>{item.product}</td>
-            <td key={i}>{item.stocklevel}</td>
-            <td key={i}>{item.price}</td>
+          <tr key={i}>
+            <td>{item.name}</td>
+            <td>{item.product}</td>
+            <td>{item.stocklevel}</td>
+            <td>{item.price}</td>
           </tr>
         ))}
       </table>
